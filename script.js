3db904bb9773de541b4cec1e86e14a7b5036900a
Pegada Sai Praneeth <saipraneethpegada@gmail.com>
2022-09-23 22:49:12 +0530

    responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let container = document.createElement("div");
+container.setAttribute(
+    "class",
+    "content container d-flex flex-column justify-content-start"
+);
+
 let heading = document.createElement("h1");
 heading.setAttribute("class", "text-center p-4");
 heading.innerHTML = "Random Programming Quote";
-document.body.append(heading);
 
 let div = document.createElement("div");
 div.setAttribute("id", "maincontainer");
 div.setAttribute("class", "d-flex justify-content-center align-items-center");
-document.body.append(div);
+container.appendChild(heading);
+container.appendChild(div);
+document.body.append(container);
 
 let url = "https://programming-quotes-api.herokuapp.com/quotes/random";
 
@@ -25,14 +32,14 @@ const getData = async () => {
 getData();
 
 const displayData = (obj) => {
-    maincontainer.innerHTML += `<div class="card rounded-2">
+    maincontainer.innerHTML += `<div class="card rounded-2 m-5">
     <div class="card-header d-flex justify-content-between align-items-center">
     <div>
         Tech Quote
     </div>
     <cite title="New Quote"><button onClick="window.location.reload()" class='refresh bg-white border-0 rounded-3 text-right'><img src='./refresh.PNG' alt='refresh'></button></cite>
     </div>
-    <div class="card-body">
+    <div class="card-body p-4">
       <blockquote class="blockquote mb-0">
         <p class='p-2'>${obj.en}</p>
         <footer class="blockquote-footer text-end mx-3 p-3"><cite title="Author">${obj.author}</cite></footer>
@@ -43,4 +50,4 @@ const displayData = (obj) => {
 
 setTimeout(() => {
     window.location.reload();
-}, 1000 * 60);
+}, 1000 * 60 * 60);
